a22f40983c94124e6a2066cfdc9cf024b44112a3
silentw0lf <clemens10@gmx.net>
2023-01-03 12:31:07 +0100

    07

---
+++ 07/solve.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+with open("input.txt") as f:
+    commands = f.readlines()
+
+sizes = defaultdict(int)
+stack = []
+
+i = 0
+for c in commands:
+    if c.startswith("$ ls") or c.startswith("dir"):
+        continue
+    if c.startswith("$ cd"):
+        dest = c.split()[2]
+        if dest == "..":
+            stack.pop()
+        else:
+            i += 1
+            path = f"{stack[-1]}_{dest}" if stack else dest
+            stack.append(path)
+    else:
+        size, file = c.split()
+        for path in stack:
+            sizes[path] += int(size)
+
+needed_size = 30000000 - (70000000 - sizes["/"])
+for size in sorted(sizes.values()):
+    if size > needed_size:
+        break
+
+print(sum(n for n in sizes.values() if n <= 100000)) # task 1
+print(size) # task 2
